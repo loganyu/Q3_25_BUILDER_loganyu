202268b51db2d935e87a3deee6e9cdcf1a836730
Logan Yu <yu.logan@gmail.com>
2025-07-10 14:58:49 -0400

    add amm

--- amms/amm/programs/amm/src/lib.rs
+++ amms/amm/programs/amm/src/lib.rs
@@ -1,4 +1,12 @@
+#![allow(unexpected_cfgs, deprecated)]
+
 use anchor_lang::prelude::*;
+
+mod instructions;
+mod state;
+
+use instructions::*;
+use state::*;
 
 declare_id!("2f7aXiUHdnAhuBuPLA7wHwaDXf5hkGCnZH1wT9YgpeKY");
 
@@ -6,11 +14,25 @@ declare_id!("2f7aXiUHdnAhuBuPLA7wHwaDXf5hkGCnZH1wT9YgpeKY");
 pub mod amm {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(
+        ctx: Context<Initialize>,
+        seed: u64,
+        fee: u16,
+        authority: Option<Pubkey>,
+    ) -> Result<()> {
+        ctx.accounts.init(seed, fee, authority, ctx.bumps)?;
+
+        Ok(())
+    }
+
+    pub fn deposit(
+        ctx: Context<Deposit>,
+        amount: u64,
+        max_x: u64,
+        max_y: u64,
+    ) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)?;
+
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
