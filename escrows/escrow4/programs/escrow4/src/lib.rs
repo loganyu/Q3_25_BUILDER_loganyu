a69ba526456d6c7efd4981a3e869f992afff20e9
Logan Yu <yu.logan@gmail.com>
2025-08-01 18:55:16 -0400

    escrow updates

--- escrows/escrow4/programs/escrow4/src/lib.rs
+++ escrows/escrow4/programs/escrow4/src/lib.rs
@@ -1,4 +1,11 @@
+#![allow(unexpected_cfgs, deprecated)]
 use anchor_lang::prelude::*;
+
+pub mod instructions;
+pub mod state;
+
+use instructions::*;
+use state::*;
 
 declare_id!("6tUm5wq3ttg81FX7d93Bs4zssP35fj9KDacwnkxMveB9");
 
@@ -6,11 +13,15 @@ declare_id!("6tUm5wq3ttg81FX7d93Bs4zssP35fj9KDacwnkxMveB9");
 pub mod escrow4 {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps);
+        ctx.accounts.deposit(deposit);
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Make>, seed: u64, deposit: u64, receive: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive, &ctx.bumps);
+        ctx.accounts.deposit(deposit);
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
